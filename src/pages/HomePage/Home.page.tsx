084ad73a3408158e3b528e8559579af21818b260
Minkaill <mkx.dake@gmail.com>
2023-11-26 18:28:02 +0300

    Добавил изменение темы

--- src/pages/HomePage/Home.page.tsx
+++ src/pages/HomePage/Home.page.tsx
@@ -2,8 +2,16 @@ import { InputWithButton } from "@/components/InputWithButton/InputWithButton";
 import classes from "./Home.page.module.css"
 import lectureItems from "../../database/lecture_items.json"
 import { ArticleCardVertical } from "@/components/ArticleCardVertical/ArticleCardVertical";
+import { useEffect } from "react";
+import { useMantineColorScheme } from "@mantine/core";
 
 export function HomePage() {
+  const { setColorScheme } = useMantineColorScheme()
+
+  useEffect(() => {
+    setColorScheme("dark")
+  }, [setColorScheme])
+
   return (
     <div className={classes.wrapper}>
       <InputWithButton />
